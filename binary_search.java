f2edc36e6b18773c85987577e54e3ee39ef97ee9
Unknown <328431798@qq.com>
2017-11-09 11:02:14 -0500

    find peak element
    When the middlle point is found, which element is compared with the middle point? How to update the start and end index of the interval.

--- binary_search.java
+++ binary_search.java
@@ -206,3 +206,48 @@ public class Solution {
         return nums[l] < nums[r] ? nums[l]:nums[r];
     }
 }
+
+
+// find the peak element
+/*
+ * nums[0] < nums[1] && nums[n-2] > nums[n-1]
+ * If it is the peak element, then nums[p] > nums[p-1] && nums[p] > nums[p+1]
+ * We need to figure out which half is discarded. We observe that the left half of
+ * the peak element is ascending order, the right half is descending order.
+ */
+//divide into two cases
+public class Solution {
+    public int findPeakElement(int[] nums) {
+        int start=0,end=nums.length-1;
+        int mid;
+        while(start+1<end){
+            mid=start+(end-start)/2;
+            if(nums[mid]<nums[mid+1] && nums[mid] > nums[mid-1]) start=mid;
+            else end=mid;
+        }
+        return nums[start]>nums[end]? start:end;
+
+    }
+}
+//divide into 4 cases
+public class Solution {
+    /*
+     * @param A: An integers array.
+     * @return: return any of peek positions.
+     */
+    public int findPeak(int[] A) {
+        // write your code here
+        if(A.length == 0) return 0;
+        int start = 0, end = A.length - 1;
+        while(start + 1 < end) {
+            int mid = start + (end - start) / 2; 
+            if(A[mid] > A[mid - 1] && A[mid] > A[mid + 1]) return mid; // if it is the peak value
+            else if(A[mid] > A[mid + 1]) end = mid; // if it is descending
+                else if(A[mid] < A[mid + 1]) start = mid; // if it is ascending
+                     else start = mid; // if it is the minimum
+        }
+        if(start > 0 && A[start] > A[end] ) return start;
+        if(end < A.length - 2 && A[end] > A[start]) return end;
+        return -1;
+    }
+}
