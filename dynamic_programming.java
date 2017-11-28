b5d2ef2f7040a73f4eb65a08c965f0257c22ae01
Unknown <328431798@qq.com>
2017-11-28 17:33:31 -0500

    Russion doll envelopes
    dp, bianary search, sort, comparator

--- dynamic_programming.java
+++ dynamic_programming.java
@@ -8,7 +8,7 @@
 
 /*First approach*/
 /*
- * step 1st: initialize an array to store intermediate result. Purpose it to avoid duplicate computation.
+ * step 1st: initialize an array to store intermediate result. Purpose is to avoid duplicate computation.
  * step 2nd: write the for loop logic, how to utilize the previous result to update the array.
  * use extra space to store the max 
  * be careful whether the updated value is greater than the existed val, otherwise the correct one
@@ -17,12 +17,13 @@
 
 public class Solution {
 	public int longestIncreasingSubsequence (int[] nums){
-		int[] result = new int[nums.length];
+		int[] result = new int[nums.length]; //store the count, update the max in the count array.
 		int max = Integer.MIN_VALUE;
 		for(int i = 0; i < nums.length; i++) {
 			result[i] = 1;
 			for(int j = 0; j < i; j++){
 				result[i] = result[i] > result[j] + 1 ? result[i]: result[j] + 1;
+				//result[i] = result[j] + 1 will fail on use case: 9, 3, 6, 2, 7
 			}
 			if(result[i] > max) {
 				max = result[i];
@@ -36,7 +37,9 @@ public class Solution {
 /*Second approach*/
 /*
  * Use binary search to update the intermediate result.
- * The intermediate array is designed to store the length of last longest increasing subsequence.
+ * The intermediate array is designed to store the actual element in the input array instead of the count.
+ * The saved elements is in sorted order and will alrways derive the length of longest increasing subsequence.
+ * Key point is how to insert the element into this array and how to initialize the array.
  * Try to find the position in the lastMin array to insert the current element in nums.
  * The index of the last element which value is not Integer.MAX_VALUE is the result.
  */
@@ -70,5 +73,52 @@ public class Solution {
 			}
 		}
 		return end;
+	}
+}
+
+
+//Russian Doll Envelopes
+/*Binary Search, Dynamic Programming*/
+/*
+ * Follow-up and advanced version of LIS. 
+ * Key point is how to SORT the envelopes, how to insert into the correct position.
+ * The width is sorted in ascending order, if the width is the same, orted in "descending" order.
+ * Iterate and do the same thing as in LIS on the height of envelopes.
+ * Use Java Built-in binary search method.
+ * Use Comparator to sort
+ */
+
+public class Solution {
+	public int maxEnvelopes(int[][] envelopes) {
+		if(envelopes.length == 0 || envelopes == null
+			|| envelopes[0] == null || envelopes[0].length != 2) {
+			return 0;
+		}
+		Arrays.sort(envelopes, new Comparator<int[]>(){
+			public int compare(int[] arr1, int[] arr2){
+				if (arr1[0] == arr2[0]) {
+					return arr2[1] - arr1[1];
+				} else {
+					return arr1[0] - arr2[0];
+				}
+			}
+		});
+		int[] dp = new int[envelopes.length];
+		length = 0;
+		for (int[] envelop : envelopes) {
+			int index = Arrays.binarySearch(dp, 0, len, envelop[1]); // we search in range [0, len)
+			if (index < 0){
+				index = -index - 1;
+			} // we didn't find the envelop[1]
+			dp[index] = envelop[1]; 
+			// if envelop is the largest, we add it at the tail(index = len)
+			// otherwise, determine the insert position, update the dp array 
+			if (length == index) {
+				length ++; 
+			//update the length only if the length is equal to the index, mean it is the largest.
+			}
+		}
+
+		return length;
 	}
 }
