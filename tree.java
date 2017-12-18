17ae4f72970918a2c44e93760ae72a0927f8b70d
Unknown <328431798@qq.com>
2017-12-18 18:33:01 -0500

    Search range in BST, subtree

--- tree.java
+++ tree.java
@@ -1228,3 +1228,64 @@ public class Solution {
         return isFullTree(root.left) && isFullTree(root.right);
     }
 }
+
+// Search Range in Binary Search Tree
+/*
+ * BST
+ * divide and conquer
+ * call recursion according to BST property
+ */
+public class Solution {
+    /*
+     * @param root: param root: The root of the binary search tree
+     * @param k1: An integer
+     * @param k2: An integer
+     * @return: return: Return all keys that k1<=key<=k2 in ascending order
+     */
+    public List<Integer> searchRange(TreeNode root, int k1, int k2) {
+        // write your code here
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+        List<Integer> left = null;
+        List<Integer> right = null;
+        if (root.val >= k1 ) {
+            left = searchRange(root.left, k1, k2);
+        }
+        if (root.val <= k2) {
+            right = searchRange(root.right, k1, k2);
+        }
+        if (left != null) res.addAll(left);
+        if (root.val >= k1 && root.val <= k2) {
+            res.add(root.val);
+        }
+        if (right != null) res.addAll(right);
+        return res;
+    }
+    
+}
+
+// Subtree
+/*
+ * Need helper function to check if two trees are equal
+ */
+public class Solution {
+    /*
+     * @param T1: The roots of binary tree T1.
+     * @param T2: The roots of binary tree T2.
+     * @return: True if T2 is a subtree of T1, or false.
+     */
+    public boolean isSubtree(TreeNode T1, TreeNode T2) {
+        // write your code here
+        if (T2 == null) return true;
+        if (T1 == null) return false;
+        if (isEqual(T1,T2)) return true;
+        else return isSubtree(T1.left, T2) || isSubtree(T1.right, T2);
+        
+    }
+    
+    public boolean isEqual(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) return true;
+        else if (t1 == null || t2 == null) return false;
+        return (t1.val == t2.val) && isEqual(t1.left, t2.left) && isEqual(t1.right, t2.right);
+    }
+}
