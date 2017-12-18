f944d58599feeb01cfdede20de62f4a2b92527d0
Unknown <328431798@qq.com>
2017-12-18 17:48:42 -0500

    clone bianry tree, check full binary tree
    divide and conquer

--- tree.java
+++ tree.java
@@ -1194,3 +1194,37 @@ public class Solution {
         
     }
 }
+
+// Clone Binary Tree
+/* Divide and Conquer*/
+public class Solution {
+    /*
+     * @param root: The root of binary tree
+     * @return: root of new tree
+     */
+    public TreeNode cloneTree(TreeNode root) {
+        // write your code here
+        if (root == null) return null;
+        TreeNode node = new TreeNode(root.val);
+        node.left = cloneTree(root.left);
+        node.right = cloneTree(root.right);
+        return node;
+    }
+}
+
+// Check Full Binary Tree
+/* Divide and Conquer*/
+public class Solution {
+    /*
+     * @param : the given tree
+     * @return: Whether it is a full tree
+     */
+    public boolean isFullTree(TreeNode root) {
+        // write your code here
+        if (root == null) return true;
+        if (root.left == null && root.right != null) return false;
+        if (root.right == null && root.left != null) return false;
+        if (root.left == null && root.right == null) return true;
+        return isFullTree(root.left) && isFullTree(root.right);
+    }
+}
