73ad7d9ed0879380c70ec07973efdea84ff24773
Unknown <328431798@qq.com>
2018-01-03 16:18:02 -0500

    remove duplicates from sorted array I, II
    Two pointers,  O(1) space complexity, O(n) time complexity, lock the tail position

--- array.java
+++ array.java
@@ -31,3 +31,58 @@ public class Solution {
         return max;
     }
 }
+
+//Remove Duplicates from Sorted Array
+/*Two Pointers, Multiple duplicates may exist, IN-PLACE*/
+/*Use extra variable to keep track of the tail of new array.
+ *Override the array and move element forward if it is unique.
+ *All duplicates will be overrided.
+ */
+public class Solution {
+    /*
+     * @param nums: An ineger array
+     * @return: An integer
+     */
+    public int removeDuplicates(int[] nums) {
+        // write your code here
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int tail = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[tail]) {
+                nums[++tail] = nums[i];
+            }
+        }
+        return tail + 1;
+        
+    }
+}
+
+//Remove Duplicates from Sorted Array II 
+/* Allow duplicates at most twice, need to count the times of duplicates*/
+public class Solution {
+    /**
+     * @param A: a array of integers
+     * @return : return an integer
+     */
+    public int removeDuplicates(int[] nums) {
+        // write your code here
+        if (nums.length == 0 || nums == null) return 0;
+        int tail = 0;
+        int flag = 1; // count duplicates
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[tail] ) {
+                nums[++tail] = nums[i];
+                flag = 1;
+            }else {
+                if (flag < 2) { // the second time
+                    nums[++tail] = nums[i]; // BE CAREFUL, ERROR IF ONLY INCREMENT TAIL
+                    flag ++;
+                }
+            }
+            
+        }
+        return tail + 1;
+    }
+}
